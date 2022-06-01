c9ae9f1319cd01c914b5a1898f80d908a5f3a606
Christopher Wilke <christopher.wilke86@googlemail.com>
2022-06-01 11:25:47 +0000

    Read config file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use rust_azure_iot_simulator::{sender::send_d2c_message};
+use log::debug;
+use rust_azure_iot_simulator::{configuration::{get_deserialized_configuration}};
 
 #[tokio::main]
 async fn main() {
     // Initialize logger
     env_logger::init();
 
+    // Read `base.yaml` from `configuration` folder
+    let config = get_deserialized_configuration().expect("Could not load config file");
+
+    debug!("{}", config.iothub.hostname);
+
     // let mut device_simulator = DeviceSimulator::default();
     // device_simulator.start().await;
 
-    send_d2c_message().await;
+    // send_d2c_message().await;
+
+  
 }
