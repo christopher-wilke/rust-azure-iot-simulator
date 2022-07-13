de1a625f599736451c553cd39dbf3628cc7c62bd
Christopher Wilke <christopher.wilke86@googlemail.com>
2022-07-13 14:24:52 +0200

    added temperature we app

---
+++ temperature-visualizer/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import ApplicationBar from './layout/ApplicationBar';
+
+function App() {
+  return (
+    <div className="App">
+      <ApplicationBar />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload! :D
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
